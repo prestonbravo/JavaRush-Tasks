62caff2b8401e7c95557fc6ba30794955e64135d
Valery <huddsonmark@gmail.com>
2024-09-12 00:22:48 +0300

    Update Java Syntax Pro Level 2-3.java

--- Java Syntax Pro Level 2/Java Syntax Pro Level 2-3.java
+++ Java Syntax Pro Level 2/Java Syntax Pro Level 2-3.java
@@ -1,1 +1,52 @@
+// Task 1.
+package com.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner console = new Scanner(System.in);
+    String name = console.nextLine();
+    String namea = console.nextLine();
+    String nameb = console.nextLine();
+    
+    System.out.println(nameb);
+    System.out.println(namea.toUpperCase());
+    System.out.println(name.toLowerCase());
+    
+    //напишите тут ваш код
+
+    }
+
+// Task 2.
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner console = new Scanner(System.in);
+    int a = console.nextInt();
+    int b = console.nextInt();
+    int c = console.nextInt();
+    
+    System.out.println((a + b+ c) / 3);
+    
+    
+    //напишите тут ваш код
+
+    }
+}
+
+
 
