d74dedb2c58427b494694e7e74186c20352d972f
Valery <huddsonmark@gmail.com>
2024-09-11 21:19:15 +0300

    Create Java Syntax Pro Level 2-1.java

---
+++ Java Syntax Pro Level 2/Java Syntax Pro Level 2-1.java
@@ -0,0 +1,102 @@
+/* Task 1. Assign them different values. Use a single command to create and initialize variables.
+
+The values can be any integers*/
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    int intValue = 7, numericValue = 5, size = 12,number = 91;    //напишите тут ваш код
+
+    }
+}
+
+//Task 2
+package com.javarush.task.pro.task02.task0202;
+
+/* 
+Полнометражная картина
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = centimeters / 100; //напишите тут ваш код
+
+        System.out.println(meters);
+    }
+}
+
+//Task 3. Last digit.
+package com.javarush.task.pro.task02.task0203;
+
+/* 
+Последняя цифра числа
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int number = 546;
+
+        int lastDigit = number % 10; //напишите тут ваш код
+
+        System.out.println(lastDigit);
+    }
+}
+
+//Task 4
+package com.javarush.task.pro.task02.task0204;
+
+/* 
+9-ка это перевернутая 6-ка
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int six = 6;
+
+    six++;
+    six++;
+    six++;//напишите тут ваш код
+
+
+        System.out.println(six);
+    }
+}
+
+//Task 5.
+package com.javarush.task.pro.task02.task0205;
+
+/* 
+Когда-то было 32 зуба
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int toothCounter = 32;
+
+       toothCounter--;
+       toothCounter--;
+       toothCounter--;
+       toothCounter--;
+       toothCounter--;
+       toothCounter--;
+       toothCounter--;
+       toothCounter--;
+       toothCounter--;//напишите тут ваш код
+
+
+        System.out.println(toothCounter);
+    }
+}
+
+//
